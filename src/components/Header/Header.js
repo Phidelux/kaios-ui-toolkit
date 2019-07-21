6529dbc048d17a8cc17da447d31f710038123a18
Andreas Schickedanz <as@bornageek.com>
2019-07-21 22:37:12 +0200

    Reworked header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
 import React from "react"
 import PropTypes from 'prop-types';
 
+import css from "./Header.module.css";
+
 // HINT: Component does not rerender if props did not change.
 const Header = React.memo(
 	({title}) => {
 		return (
-			<h1 className="app-header">{title}</h1>
+			<h1 className={css.app_header}>{title}</h1>
 		)
 	}
 );
