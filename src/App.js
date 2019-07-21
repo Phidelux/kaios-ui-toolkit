8b483fe38753d5e40cc663cd9906c51dde4c84c9
Andreas Schickedanz <as@bornageek.com>
2019-07-21 22:41:27 +0200

    Adapted app to latest changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react"
 
-import Header from "./Header"
+import Header from "./components/Header/Header"
+import SoftKeys from "./components/SoftKeys/SoftKeys"
 
 const App = () => {
 	return (
 		<div className="app">
 			<Header title="KaiOS UI Example" />
+
+			<SoftKeys
+				leftText="Add"
+				enterText="Play"
+				rightText="Options" />
 		</div>
 	)
 }
