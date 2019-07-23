bd576153cb61f0f896bbad021e7e93b603e50657
Andreas Schickedanz <as@bornageek.com>
2019-07-23 22:22:17 +0200

    Added OptionMenu to App

--- src/App.js
+++ src/App.js
@@ -2,19 +2,32 @@ import React from "react"
 
 import Header from "./components/Header/Header"
 import SoftKeys from "./components/SoftKeys/SoftKeys"
+import OptionMenu, {OptionItem} from "./components/OptionMenu/OptionMenu"
 
 const App = () => {
 	return (
 		<div className="app">
 			<Header title="KaiOS UI Example" />
 
+			<OptionMenu>
+				<OptionItem
+					text="Add Podcast"
+					onClick={() => console.log("Add Podcast")} />
+				<OptionItem
+					text="Remove Podcast"
+					onClick={() => console.log("Remove Podcast")} />
+				<OptionItem
+					text="About"
+					onClick={() => console.log("About this App")} />
+			</OptionMenu>
+
 			<SoftKeys
 				leftText="Add"
-				onLeftClicked={() => console.log("left")}
+				onLeftClicked={() => console.log("Add")}
 				enterText="Play"
-				onEnterClicked={() => console.log("enter")}
+				onEnterClicked={() => console.log("Play")}
 				rightText="Options"
-				onRightClicked={() => console.log("right")} />
+				onRightClicked={() => console.log("Options")} />
 		</div>
 	)
 }
