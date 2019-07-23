a81984c27cb2e54133941471c03ffab49de274c6
Andreas Schickedanz <as@bornageek.com>
2019-07-23 22:21:07 +0200

    Several fixes and enhancements of OptionMenu

--- src/components/OptionMenu/OptionMenu.js
+++ src/components/OptionMenu/OptionMenu.js
@@ -1,31 +1,76 @@
 import React, { useState, useEffect, useCallback }  from "react"
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
 import css from "./OptionMenu.module.css";
 
+export const OptionItem = React.forwardRef(
+	(props, ref) => {
+		const onFocusChange = props.onFocusChange;
+		const [isFocused, setFocus] = useState(false);
+
+		const handleFocusChange = (newFocus) => {
+			setFocus(newFocus);
+			if(newFocus) {
+				onFocusChange(props.index)
+			}
+		};
+
+		return (
+			<div
+				tabIndex="0"
+				ref={ref}
+				onFocus={() => handleFocusChange(true)}
+				onBlur={() => handleFocusChange(false)}
+				style={{ backgroundColor: isFocused ? 'red' : 'white' }}
+				className={props.className ? props.className : null}>
+				{props.text}
+			</div>
+		);
+	}
+);
+
+OptionItem.propTypes = {
+	type: PropTypes.string,
+	index: PropTypes.number,
+	text: PropTypes.string.isRequired,
+	ref: PropTypes.oneOfType([
+		PropTypes.func,
+		PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
+	]),
+	onFocusChange: PropTypes.func
+};
+
+OptionItem.defaultProps = {
+	type: "OptionItem",
+	onClick: () => {}
+};
+
 const OptionMenu = (props) => {
-	const optionItems = props.children;
+	const itemRefs = [];
+	const onIndexChanged = props.onIndexChanged;
 	const onItemSelected = props.onItemSelected;
 	const [selectedItem, setSelectedItem] = useState(0);
 
-	const requestFocus =  useCallback(
-		(index) => {
-			if(typeof optionItems !== undefined && optionItems.length > 0) {
-				setSelectedItem(index);
-				optionItems[selectedItem].focus();
+	const requestFocus = useCallback(
+		index => {
+			if(typeof itemRefs !== undefined && itemRefs.length > 0) {
+				ReactDOM.findDOMNode(itemRefs[index].current).focus();
+
+				onIndexChanged(index);
 			}
 		},
-		[setSelectedItem, optionItems]
+		[itemRefs, onIndexChanged]
 	);
 
 	const onKeyPressed = useCallback(
 		({key}) => {
 			switch(key) {
 				case 'ArrowUp':
-					requestFocus(prevItem => selected > 0 ? --selected : optionItems.length - 1);
+					setSelectedItem(prevItem => prevItem > 0 ? --prevItem : itemRefs.length - 1);
 					break;
 				case 'ArrowDown':
-					requestFocus(prevItem => prev < optionItems.length ? prev + 1 : 0);
+					setSelectedItem(prevItem => prevItem < itemRefs.length - 1 ? prevItem + 1 : 0);
 					break;
 				case 'Enter':
 					onItemSelected(selectedItem);
@@ -34,26 +79,33 @@ const OptionMenu = (props) => {
 					break;
 			}
 		},
-		[requestFocus, onItemSelected],
+		[itemRefs, selectedItem, onItemSelected]
 	);
+
+	useEffect(() => requestFocus(selectedItem), [requestFocus, selectedItem]);
 
 	useEffect(
 		() => {
-			setSelectedItem(0);
-
 			document.addEventListener('keydown', onKeyPressed);
 
 			return () => document.removeEventListener('keydown', onKeyPressed);
-		}, [optionItems]
+		}, [onKeyPressed]
 	);
 
 	const renderedItems = React.Children.map(
-		optionItems, (item, idx) => React.cloneElement(
-			item, {
-				index: idx,
-				onFocusChange: this.onIndexChanged
-			}
-		)
+		props.children, (item, idx) => {
+			const childRef = React.createRef();
+
+			itemRefs[idx] = childRef;
+
+			return React.cloneElement(
+				item, {
+					index: idx,
+					onFocusChange: requestFocus,
+					ref: childRef
+				}
+			);
+		}
 	);
 
 	return (
@@ -70,6 +122,7 @@ OptionMenu.propTypes = {
 	type: PropTypes.string,
 	header: PropTypes.string,
 	onIndexChanged: PropTypes.func,
+	onItemSelected: PropTypes.func,
 	children: PropTypes.arrayOf(
 		PropTypes.shape({
 			text: PropTypes.string,
@@ -81,7 +134,8 @@ OptionMenu.propTypes = {
 OptionMenu.defaultProps = {
 	type: "OptionMenu",
 	header: "Options",
-	onIndexChanged: () => {},
+	onIndexChanged: (index) => { console.log(`OptionMenu: Item ${index} focused`) },
+	onItemSelected: (index) => { console.log(`OptionMenu: Item ${index} selected`) },
 	children: []
 };
 
